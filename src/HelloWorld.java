46df4734fc68ebd12218fcb8a2e92d05615e8c8c
Dave Thompson <david.thompson@capstonec.com>
2017-07-26 18:08:30 -0600

    Changing helloworld output #2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("Hello world chap 6, for build #2");
+		System.out.println("Hello world chap 6, for build #3");
 	}
 }
